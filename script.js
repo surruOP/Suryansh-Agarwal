9a8dc9eb3d97588f1f527758cb9b0835310d9ff3
surruOP <134080288+surruOP@users.noreply.github.com>
2023-05-20 11:23:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const BLACKLISTED_KEY_CODES = [ 38 ];
+const COMMANDS = {
+  help:
+    'Supported commands: <span class="code">about</span>, <span class="code">experience</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">certifications</span>, <span class="code">contact</span>',
+  about:
+    "web developer and video editor capable of building static as well as responsive website for your business. I also have experience in video editing and image editing and enhancing as well as professional photographer and good content developer .",
+  skills:
+    "Video Editing | Photo Editing| Web Developer | Content Developer",
+  education:
+    "Bachelor in arts in history, social science, political science <br> ",
+  experience:'<span class="code">Total experience: As a Math Tutor, Proctor, Content Creator for 1 Years At an edtech company </span><br> ',
+  certifications: 
+    "Internshala Web development Course<br> ",
+  contact:
+    "You can contact me on Linkedin and or via the mail:<br><a href='linkedin.com/in/suryansh-agarwal-21952b277' class='success link'>Linkedin</a>, <a href='mailto:suryanshagarwal101@gmail.com' class='success link'>Email</a>,"
+};
+let userInput, terminalOutput;
+
+const app = () => {
+  userInput = document.getElementById("userInput");
+  terminalOutput = document.getElementById("terminalOutput");
+  document.getElementById("dummyKeyboard").focus();
+  console.log("Application loaded");
+};
+
+const execute = function executeCommand(input) {
+  let output;
+  input = input.toLowerCase();
+  if (input.length === 0) {
+    return;
+  }
+  output = `<div class="terminal-line"><span class="success">➜</span> <span class="directory">~</span> ${input}</div>`;
+  if (!COMMANDS.hasOwnProperty(input)) {
+    output += `<div class="terminal-line">no such command: ${input}</div>`;
+    console.log("Oops! no such command");
+  } else {
+    output += COMMANDS[input];
+  }
+
+  terminalOutput.innerHTML = `${
+    terminalOutput.innerHTML
+  }<div class="terminal-line">${output}</div>`;
+  terminalOutput.scrollTop = terminalOutput.scrollHeight;
+};
+
+const key = function keyEvent(e) {
+  const input = userInput.innerHTML;
+
+  if (BLACKLISTED_KEY_CODES.includes(e.keyCode)) {
+    return;
+  }
+
+  if (e.key === "Enter") {
+    execute(input);
+    userInput.innerHTML = "";
+    return;
+  }
+
+  userInput.innerHTML = input + e.key;
+};
+
+const backspace = function backSpaceKeyEvent(e) {
+  if (e.keyCode !== 8 && e.keyCode !== 46) {
+    return;
+  }
+  userInput.innerHTML = userInput.innerHTML.slice(
+    0,
+    userInput.innerHTML.length - 1
+  );
+};
+
+document.addEventListener("keydown", backspace);
+document.addEventListener("keypress", key);
+document.addEventListener("DOMContentLoaded", app);
